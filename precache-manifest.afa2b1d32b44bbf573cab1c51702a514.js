7f44f24df4f99a4bbf6c9ce10aba92aea81c1c7f
kapilasthana2019 <kapilasthana2019@gmail.com>
2020-08-08 11:18:18 +0530

    Updates

---
+++ precache-manifest.afa2b1d32b44bbf573cab1c51702a514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f761b5a309532ac51d76646670d57f",
+    "url": "/SearchMovies/index.html"
+  },
+  {
+    "revision": "0c7ddf562d1b5c880d53",
+    "url": "/SearchMovies/static/css/main.46a207f5.chunk.css"
+  },
+  {
+    "revision": "9fd14b6a0cb88ce64fe0",
+    "url": "/SearchMovies/static/js/2.d14811f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SearchMovies/static/js/2.d14811f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7ddf562d1b5c880d53",
+    "url": "/SearchMovies/static/js/main.a653ad44.chunk.js"
+  },
+  {
+    "revision": "82e8c6ac49f624c19eda",
+    "url": "/SearchMovies/static/js/runtime-main.5cf0a1e7.js"
+  },
+  {
+    "revision": "d7dba178da0aea44d60e0fc871c6d056",
+    "url": "/SearchMovies/static/media/srk.d7dba178.jpg"
+  }
+]);
